251a52c2c5e99b47e3c34d806f5ef5845c930b20
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-13 09:27:11 -0400

    Setup xcode project

---
+++ iPadTrait/iPadTrait/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPadTrait
+//
+//  Created by Nicholas Burcin on 10/13/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
